36b3424bf8316148255074500c32fa2af77efa5e
Kleber de Menezes Thanasio <klebermt@gmail.com>
2023-07-30 15:43:38 -0300

    DB - DML

--- 34-db-mysql/go.mod
+++ 34-db-mysql/go.mod
@@ -3,6 +3,6 @@ module database-mysql
 go 1.20
 
 require (
+	github.com/bxcodec/faker/v3 v3.8.1
 	github.com/go-sql-driver/mysql v1.7.1
-	github.com/google/uuid v1.3.0
 )
--- 34-db-mysql/main.go
+++ 34-db-mysql/main.go
@@ -2,9 +2,10 @@ package main
 
 import (
 	"database/sql"
+	"fmt"
 
+	"github.com/bxcodec/faker/v3"
 	_ "github.com/go-sql-driver/mysql"
-	"github.com/google/uuid"
 )
 
 type Product struct {
@@ -13,12 +14,14 @@ type Product struct {
 	Price float64
 }
 
-func NewProduct(name string, price float64) *Product {
-	return &Product{
-		ID:    uuid.New().String(),
-		Name:  name,
-		Price: price,
+func NewProduct() *Product {
+
+	product := &Product{}
+	err := faker.FakeData(&product)
+	if err != nil {
+		panic(err)
 	}
+	return product
 }
 
 func main() {
@@ -27,8 +30,30 @@ func main() {
 		panic(err)
 	}
 	defer db.Close()
-	calca := NewProduct("Vestido", 1299.99)
-	insertProduct(db, calca)
+	product := NewProduct()
+	insertProduct(db, product)
+	product.Name = product.Name + " updated"
+	updateProduct(db, product)
+
+	p, err := selectProduct(db, product.ID)
+	if err != nil {
+		panic(err)
+	}
+	fmt.Printf("Product: id[%v] - Price: [%.2f] - Name: [%v]\n", p.ID, p.Price, p.Name)
+
+	products, err := selectProducts(db)
+	if err != nil {
+		panic(err)
+	}
+
+	for _, p := range *products {
+		fmt.Printf("Name: [%v] - Price: %.2f\n", p.Name, p.Price)
+	}
+
+	err = deleteProduct(db, product.ID)
+	if err != nil {
+		panic(err)
+	}
 }
 
 func insertProduct(db *sql.DB, product *Product) {
@@ -41,4 +66,63 @@ func insertProduct(db *sql.DB, product *Product) {
 	if err != nil {
 		panic(err)
 	}
+}
+
+func updateProduct(db *sql.DB, product *Product) {
+	stmt, err := db.Prepare("update products set name = ? ,price = ? where id = ?")
+	if err != nil {
+		panic(err)
+	}
+	defer stmt.Close()
+	_, err = stmt.Exec(product.Name, product.Price, product.ID)
+	if err != nil {
+		panic(err)
+	}
+}
+
+func selectProduct(db *sql.DB, id string) (*Product, error) {
+	stmt, err := db.Prepare("select id, name, price from products where id = ?")
+	if err != nil {
+		return nil, err
+	}
+	defer stmt.Close()
+	var p Product
+	err = stmt.QueryRow(id).Scan(&p.ID, &p.Name, &p.Price)
+	if err != nil {
+		return nil, err
+	}
+	return &p, nil
+
+}
+
+func selectProducts(db *sql.DB) (*[]Product, error) {
+	rows, err := db.Query("select id, name, price from products order by price desc")
+	if err != nil {
+		return nil, err
+	}
+	defer rows.Close()
+	var products []Product
+	for rows.Next() {
+		var p Product
+		err = rows.Scan(&p.ID, &p.Name, &p.Price)
+		if err != nil {
+			return nil, err
+		}
+		products = append(products, p)
+	}
+	return &products, nil
+}
+
+func deleteProduct(db *sql.DB, id string) error {
+	stmt, err := db.Prepare("delete from products where id = ?")
+	if err != nil {
+		return err
+	}
+	defer stmt.Close()
+	_, err = stmt.Exec(id)
+	if err != nil {
+		return err
+	}
+	return nil
+
 }
