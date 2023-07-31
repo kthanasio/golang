ca39a5a23ac2c72fb5a6f8eb4ddcd67e6b69ca9e
Kleber de Menezes Thanasio <klebermt@gmail.com>
2023-07-31 01:26:48 -0300

    Testing Basic

---
+++ 36-testing/1-testing-basic/tax/tax_test.go
@@ -0,0 +1,25 @@
+package tax
+
+import "testing"
+
+func TestCalculateTax(t *testing.T) {
+	amount := 500.0
+	expected := 5.0
+
+	result := CalculateTax(amount)
+
+	if result != expected {
+		t.Errorf("Expected %f but got %f", expected, result)
+	}
+}
+
+func TestCalculateTaxSuc(t *testing.T) {
+	amount := 1000.0
+	expected := 10.0
+
+	result := CalculateTax(amount)
+
+	if result != expected {
+		t.Errorf("Expected %f but got %f", expected, result)
+	}
+}
