e501a31b34cc41b592a941d756cb9bd97562d596
Kleber de Menezes Thanasio <klebermt@gmail.com>
2023-09-22 00:24:55 -0300

    Feat: Add context

--- 33-context/2-withtimeout-client-server/server/main.go
+++ 33-context/2-withtimeout-client-server/server/main.go
@@ -1,9 +1,15 @@
 package main
 
 import (
+	"context"
+	"fmt"
 	"log"
 	"net/http"
 	"time"
+)
+
+var (
+	apiMaxTime = 200
 )
 
 func main() {
@@ -12,14 +18,29 @@ func main() {
 }
 
 func handleRequest(w http.ResponseWriter, r *http.Request) {
+	log.Printf("Request Iniciada - Timeout[%+v]\n", time.Millisecond*time.Duration(apiMaxTime))
 	ctx := r.Context()
-	log.Println("Request Iniciada")
-	defer log.Println("Request Finalizada")
-	select {
-	case <-time.After(time.Second * 5):
-		log.Println("Request Processada com Sucesso!")
-		w.Write([]byte("Processamento Concluído com Sucesso!"))
-	case <-ctx.Done():
-		log.Println("Request Cancelada pelo Client")
+	ctx, cancel := context.WithTimeout(ctx, time.Millisecond*time.Duration(apiMaxTime))
+	response, err := Executa(&ctx)
+	if err != nil {
+		log.Fatalf("Timeout na chamada da API")
 	}
+	defer cancel()
+
+	fmt.Printf("Resultado: %v\n", response)
+	defer log.Println("Request Finalizada")
+}
+
+func Executa(ctx *context.Context) (int, error) {
+	log.Println("Vai Chamar a API")
+	req, err := http.NewRequestWithContext(*ctx, "GET", "https://economia.awesomeapi.com.br/json/last/USD-BRL", nil)
+	if err != nil {
+		return 0, err
+	}
+	resp, err := http.DefaultClient.Do(req)
+	if err != nil {
+		return 0, err
+	}
+	defer resp.Body.Close()
+	return 1, nil
 }
